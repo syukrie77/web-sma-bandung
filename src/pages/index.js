9bcaafa4438a5b7703f6e2854dc39f3854b61025
Muhammad Syukri <39161232+syukrie77@users.noreply.github.com>
2024-07-05 08:22:16 +0000

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -82,7 +82,7 @@ const IndexPage = () => (
         style={{ marginBottom: `var(--space-3)` }}
       />
       <h1>
-        Welcome to <b>Gatsby!</b>
+        Selamat Datang di <b>SMA Bandung!</b>
       </h1>
       <p className={styles.intro}>
         <b>Example pages:</b>{" "}
